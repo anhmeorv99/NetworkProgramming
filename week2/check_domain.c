28595573ed223ea81de8fe356497576a1e8a8a4b
Tuan Anh <anhmeorv99@gmail.com>
2020-10-02 11:20:52 +0700

    fix alias

--- week2/check_domain.c
+++ week2/check_domain.c
@@ -1,11 +1,13 @@
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-/*
-int main()
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //for exit(0);
+#include<sys/socket.h>
+#include<errno.h> //For errno - the error number
+#include<netdb.h>	//hostent
+#include<arpa/inet.h>
+
+void convert_ip_to_hostname(char* ip)
 {
     struct sockaddr_in sa;    
     socklen_t len;         
@@ -15,7 +17,7 @@ int main()
 
     
     sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = inet_addr("123.30.134.68");
+    sa.sin_addr.s_addr = inet_addr(ip);
     len = sizeof(struct sockaddr_in);
 
     if (getnameinfo((struct sockaddr *) &sa, len, hbuf, sizeof(hbuf), 
@@ -23,50 +25,17 @@ int main()
         printf("could not resolve hostname\n");
     }
     else {
-        printf("host=%s\n", hbuf);
-    }
-
-    return 0;                                                  
-}*/
-#include<stdio.h> //printf
-#include<string.h> //memset
-#include<stdlib.h> //for exit(0);
-#include<sys/socket.h>
-#include<errno.h> //For errno - the error number
-#include<netdb.h>	//hostent
-#include<arpa/inet.h>
-
-int hostname_to_ip(char *  , char *);
-
-int main(int argc , char *argv[])
-{
-	if(argc <2)
-	{
-		printf("Please provide a hostname to resolve \n");
-		exit(1);
-	}
-	
-	char *hostname = argv[1];
-	char ip[100];
-	
-	hostname_to_ip(hostname , ip);
-	printf("%s resolved to %s" , hostname , ip);
-	
-	printf("\n");
-	
+        printf("host = %s\n", hbuf);
+    }                                            
 }
 
-
-
-//	Get ip from domain name
- 
-
-int hostname_to_ip(char * hostname , char* ip)
+int convert_hostname_to_ip(char * hostname , char* ip)
 {
 	struct hostent *he;
 	struct in_addr **addr_list;
+
 	int i;
-		
+	char test[100];
 	if ( (he = gethostbyname( hostname ) ) == NULL) 
 	{
 		// get the host info
@@ -80,11 +49,42 @@ int hostname_to_ip(char * hostname , char* ip)
 	{
 		//Return the first one;
 		strcpy(ip , inet_ntoa(*addr_list[i]) );
-		return 0;
+		strcat(ip,"\n");
 	}
+
 	
 	return 1;
 }
+
+//int hostname_to_ip(char *  , char *);
+
+int main(int argc , char *argv[])
+{
+	if(argc <3)
+	{
+		printf("Please provide a hostname to resolve \n");
+		exit(1);
+	}
+	
+	if (atoi(argv[1])==1){
+		convert_ip_to_hostname(argv[2]);
+	}
+	else if (atoi(argv[1])==2){
+		
+		char *hostname = argv[2];
+		char ip[1000];
+		
+		convert_hostname_to_ip(hostname , ip);
+		printf("%s resolved to %s" , hostname , ip);
+		printf("\n");
+	} else
+	{
+		printf("command not found pls check again !\n");
+	}
+	
+	
+}
+
 
 
 
